5ba76ce8625ea0b7f4fc6f0959e59c81797df417
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-04 02:27:07 +0900

    test add

---
+++ src/main/java/com/rixy/javaproc/chat/chatbackend/domain/hello/model/Hello.java
@@ -0,0 +1,10 @@
+package com.rixy.javaproc.chat.chatbackend.domain.hello.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Hello {
+  private String message;
+}
