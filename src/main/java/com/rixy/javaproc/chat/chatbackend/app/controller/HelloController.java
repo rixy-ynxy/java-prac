a7ff5154746cded43306f19b3d322e16d2195adb
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-04 02:12:03 +0900

    HelloController refactoring

--- src/main/java/com/rixy/javaproc/chat/chatbackend/app/controller/HelloController.java
+++ src/main/java/com/rixy/javaproc/chat/chatbackend/app/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.rixy.javaproc.chat.chatbackend.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HelloController {
 
-  @RequestMapping(path = "/hello", method = RequestMethod.GET)
-    public String hello() {
+  @GetMapping("/hello")
+  public String hello() {
     return "Hello, world";
-    }
+  }
+
 }
